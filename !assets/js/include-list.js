612ece501b6348361c48eb153c2e0fbe5b6c9741
Jon Erik Solheim <jones@restdb.io>
2016-07-05 08:25:44 +0200

    Removed relative paths

--- !assets/js/include-list.js
+++ !assets/js/include-list.js
@@ -1,7 +1,7 @@
 
 $(function(){
-  $("#includedSidebar").load("../!assets/common/sidebar.html");
-  $("#includedHeaderCore").load("../!assets/common/header-core.html");
-  $("#includedFooterMap").load("../!assets/common/footer-map.html");
-  $("#includedFooterCore").load("../!assets/common/footer-core.html");
+  $("#includedSidebar").load("https://rawgit.com/180Labs/reviewsite/master/!assets/common/sidebar.html");
+  $("#includedHeaderCore").load("https://rawgit.com/180Labs/reviewsite/master/!assets/common/header-core.html");
+  $("#includedFooterMap").load("https://rawgit.com/180Labs/reviewsite/master/!assets/common/footer-map.html");
+  $("#includedFooterCore").load("https://rawgit.com/180Labs/reviewsite/master/!assets/common/footer-core.html");
 });
